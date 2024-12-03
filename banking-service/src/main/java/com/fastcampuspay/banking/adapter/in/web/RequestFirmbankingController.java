be97255c6e78db2e87892acfb8fcf0053dd89f7c
jaemin <jjm7655@gmail.com>
2024-12-03 20:43:06 +0900

    feat: banking-service module
    post : firmbanking service
    post : bank account create

---
+++ banking-service/src/main/java/com/fastcampuspay/banking/adapter/in/web/RequestFirmbankingController.java
@@ -0,0 +1,32 @@
+package com.fastcampuspay.banking.adapter.in.web;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fastcampuspay.banking.application.port.in.RequestFirmbankingCommand;
+import com.fastcampuspay.banking.application.port.in.RequestFirmbankingUseCase;
+import com.fastcampuspay.banking.domain.FirmbankingRequest;
+import com.fastcampuspay.common.WebAdapter;
+
+import lombok.RequiredArgsConstructor;
+
+
+@WebAdapter
+@RestController
+@RequiredArgsConstructor
+public class RequestFirmbankingController {
+    private final RequestFirmbankingUseCase requestFirmbankingUseCase;
+    @PostMapping(path = "/banking/firmbanking/request")
+    FirmbankingRequest requestFirmbanking(@RequestBody RequestFirmbankingRequest request) {
+        RequestFirmbankingCommand command = RequestFirmbankingCommand.builder()
+                .toBankName(request.getToBankName())
+                .toBankAccountNumber(request.getToBankAccountNumber())
+                .fromBankName(request.getFromBankName())
+                .fromBankAccountNumber(request.getFromBankAccountNumber())
+                .moneyAmount(request.getMoneyAmount())
+                .build();
+
+        return requestFirmbankingUseCase.requestFirmbanking(command);
+    }
+}
